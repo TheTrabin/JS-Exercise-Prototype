c8db56df3ae46e88ef5bf9021ad69825bcfe982b
TheTrabin <beyondtears@gmail.com>
2020-02-18 21:57:15 -0800

    Worked on the four problems. commented and gave sources at the bottom for things I searched for.

--- index.js
+++ index.js
@@ -39,9 +39,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.stomach = [];
+  this.name = name;
+  this.age = age;
+  
+}// Initial person object including stomach array to be used lower.
+Person.prototype.eat = function(someFood) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(someFood);
+  }
+}; // eating someFood adds to the stomach array up to 10 items, and then dissallow continued use.
 
-}
+Person.prototype.poop = function() {
+  this.stomach.length = 0;
+}; //Reduce Array to 0 upon poop.
+Person.prototype.toString = function personToString() {
+  return `${this.name} ", "${this.age}`;
+}; //return the name and age of Person upon using .poop
 
 /*
   TASK 2
@@ -57,9 +72,32 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.tank = 0; //Tank
+  this.odometer = 0; //Base Odometer/distance, reset to 0
+  this.model = model; //Type of vehicle 
+  this.milesPerGallon = milesPerGallon; // simplifying miles per gallon
 }
+
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
+}; //Classify how much on fillup per Gallons per Vehicle model.
+
+Car.prototype.drive = function(distance) {
+  let empty = this.tank * this.milesPerGallon; //from empty, the ammount of gallons vs miles per use
+  if (distance < empty) {
+    this.odometer += distance; //How far the vehicle travelled.
+    this.tank -= distance / this.milesPerGallon; // Reduce the distance by how many gallons used
+  } else {
+    this.odometer += empty; //if the car runs out of fuel, add distance based on distance.
+    this.tank -= empty / this.milesPerGallon; // Empty the tank from prior value.
+    return `I ran out of fuel at ${this.odometer} miles!`; //String of when Tank = 0
+  };
+}
+
+
+
+
 
 /*
   TASK 3
@@ -68,18 +106,26 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age, favoriteToy);
+  this.favoriteToy = favoriteToy;
+} //create the instance.
+Baby.prototype = Object.create(Person.prototype); //Make Baby a child of Person
 
-}
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`; // String of Added favorite Toy.
+};
+
+
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window/Global Binding involves the entirity of the page, or the console/window Object.
+  2. Implicit Binding: Refers to involvement directly to an object.
+  3. New Binding: Used predominantly with Constructor functions, it refers to the specific instance of the object (created) to be refered to and returned.
+  4. Explicit binding: Explicit, exact, this is this and only this.
 */
 
 
@@ -93,3 +139,10 @@ if (typeof exports !== 'undefined') {
   if (Car) { module.exports.Car = Car }
   if (Baby) { module.exports.Baby = Baby }
 }
+
+
+//Reference points: 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/getPrototypeOf
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/toString
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Math
